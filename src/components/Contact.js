cf4fd71c4ddace6219bf826aa5a4056e2865d72d
hirose <yukihio76@gmail.com>
2024-02-08 23:36:13 +0900

    お問い合わせ記事の追加

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import '../css/contact.css'
+
+const Contact = () => {
+  return(
+    <section className="container">
+      <h2>お問い合わせ</h2>
+      <div className="contact-list">
+          <div>
+            <p className="lead">
+              お問い合わせは、<span className="line">メール</span>または<span className="line">SNS</span>にてお願いいたします。
+            </p>
+            <h3>メール</h3>
+            <p>yukihio76@gmail.com</p>
+            <h3>SNS</h3>
+            <a href="https://twitter.com/otamoT10010">
+              <img src="img/icons8-x-100.png" alt="X" />
+              <p>X(旧Twitter)</p>
+            </a>
+          </div>
+          <blockquote className="twitter-tweet"><p lang="ja" dir="ltr"><a href="https://twitter.com/hashtag/%E4%BB%8A%E6%97%A5%E3%81%AE%E7%A9%8D%E3%81%BF%E4%B8%8A%E3%81%92?src=hash&amp;ref_src=twsrc%5Etfw">#今日の積み上げ</a> メモ<br />入学137日目　総学習時間505時間<br />✅Typescriptの基礎<br />少し前にNext.jsと一緒に学んでいましたが時間を置いてから学び直すと頭が整理されてわかりやすかったです。<a href="https://twitter.com/hashtag/%E9%A7%86%E3%81%91%E5%87%BA%E3%81%97%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2?src=hash&amp;ref_src=twsrc%5Etfw">#駆け出しエンジニア</a><a href="https://twitter.com/hashtag/%E9%A7%86%E3%81%91%E5%87%BA%E3%81%97%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%A8%E7%B9%8B%E3%81%8C%E3%82%8A%E3%81%9F%E3%81%84?src=hash&amp;ref_src=twsrc%5Etfw">#駆け出しエンジニアと繋がりたい</a><a href="https://twitter.com/hashtag/Typescript?src=hash&amp;ref_src=twsrc%5Etfw">#Typescript</a><a href="https://twitter.com/hashtag/%E4%BE%8D%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2?src=hash&amp;ref_src=twsrc%5Etfw">#侍エンジニア</a><a href="https://twitter.com/hashtag/%E4%BE%8D%E3%83%86%E3%83%A9%E3%82%B3%E3%83%A4?src=hash&amp;ref_src=twsrc%5Etfw">#侍テラコヤ</a></p>&mdash; ゆっきー (@otamoT10010) <a href="https://twitter.com/otamoT10010/status/1750891030944338381?ref_src=twsrc%5Etfw">January 26, 2024</a></blockquote>
+      </div>
+    </section>
+  )
+}
+export default Contact
