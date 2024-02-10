c0f5232118f7a9d847f46cd3665272c968c88572
hirose <yukihio76@gmail.com>
2024-02-10 14:32:21 +0900

    レイアウトの調整

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Menu = () => {
 
   return(
     <>
-      <li><a href="#works">作品<br/>Works</a></li>
+      <li><a href="#works" onClick={handleClick}>作品<br/>Works</a></li>
       <li><a href="#skill">スキル<br/>Skil</a></li>
       <li><a href="#about">自己紹介<br/>Self-introduction</a></li>
       <li><a href="#contact">お問い合わせ<br/>Contact</a></li>
