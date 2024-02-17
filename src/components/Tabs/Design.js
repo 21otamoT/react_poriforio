5fcb368aa08526d3299f7ee0ada7f23eeaa8a6ab
hirose <yukihio76@gmail.com>
2024-02-17 22:52:36 +0900

    画像のプリロード

--- src/components/Tabs/Design.js
+++ src/components/Tabs/Design.js
@@ -4,6 +4,7 @@ import Work4 from '../Works/Design/work4'
 import Work5 from '../Works/Design/work5';
 import Work7 from '../Works/Design/work7';
 import Work8 from '../Works/Design/work8';
+import PreloadImage from '../PreloadImage';
 
 const Design = () => {
   const [selectWork, setSelectWork] = useState(null)
@@ -16,21 +17,29 @@ const Design = () => {
     <>
     <div className="tab-contents">
       <a onClick={() => handleClick(<Work4 />)} class="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/photoshop.png"/>
         <div class="works-img"><img src="img/works/photoshop.png" alt="Photoshop" /></div>
         <p class="works-name">ITスクールのバナー</p>
         <p class="works-info">Photoshop</p>
       </a>
       <a onClick={() => handleClick(<Work7 />)} class="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/photshop2.png"/>
         <div class="works-img"><img src="img/works/photshop2.png" alt="Photoshop" /></div>
         <p class="works-name">ITスクールのバナー</p>
         <p class="works-info">Photoshop</p>
       </a>
       <a onClick={() => handleClick(<Work8 />)} class="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/samurai_kadai_002.png"/>
         <div class="works-img"><img src="img/works/samurai_kadai_002.png" alt="Figma" /></div>
         <p class="works-name">ITスクールのバナー</p>
         <p class="works-info">Figma</p>
       </a>
       <a onClick={() => handleClick(<Work5 />)} class="works-item works-item-row-end">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/xd.png"/>
         <div class="works-img"><img src="img/works/xd.png" alt="Figma" /></div>
         <p class="works-name">ITスクールのLP</p>
         <p class="works-info">Figma</p>
--- src/components/Tabs/WorksApp.js
+++ src/components/Tabs/WorksApp.js
@@ -9,6 +9,7 @@ import ReactTodo from '../Works/App/react_todo';
 import ReactThree from '../Works/App/react_three';
 import Postal from '../Works/App/js_postal';
 import Order from '../Works/App/spring_order_app';
+import PreloadImage from '../PreloadImage';
 
 const WorksApp = () => {
 const [selectWork, setSelectWork] = useState(null)
@@ -20,46 +21,64 @@ const handleClick = work => {
   return (
     <div className="tab-contents">
       <a onClick={() => handleClick(<Travel />)} class="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/hirosetravel.png"/>
         <div className="works-img"><img src="img/works/hirosetravel.png" alt="民宿予約アプリ" /></div>
         <p className="works-name">民宿予約アプリ</p>
         <p className="works-info">{`HTML/CSS/\nJavaScript/SpringBoot\n/SQL`}</p>
       </a>
       <a onClick={() => handleClick(<Order />)} className='works-item'>
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/Order.png"/>
         <div className="works-img"><img src="img/works/Order.png" alt="注文アプリ" /></div>
         <p className="works-name">注文アプリ</p>
         <p className="works-info">{`html/css/JavaScript\nSpringBoot/SQL`}</p>
       </a>
       <a onClick={() => handleClick(<Mart />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/hirose_mart.png"/>
         <div className="works-img"><img src="img/works/hirose_mart.png" alt="商品管理アプリ" /></div>
         <p className="works-name">商品管理アプリ</p>
         <p className="works-info">HTML/CSS/PHP/SQL</p>
       </a>
       <a onClick={() => handleClick(<Todo />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/todo_app.png"/>
         <div className="works-img"><img src="img/works/todo_app.png" alt="ToDoアプリ" /></div>
         <p className="works-name">ToDoアプリ</p>
         <p className="works-info">HTML/CSS/JavaScript</p>
       </a>
       <a onClick={() => handleClick(<Typing />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/typing_game.png"/>
         <div className="works-img"><img src="img/works/typing_game.png" alt="タイピングゲーム" /></div>
         <p className="works-name">タイピングゲーム</p>
         <p className="works-info">HTML/CSS/JavaScript</p>
       </a>
       <a onClick={() => handleClick(<Menu />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/react_menu_app.png"/>
         <div className="works-img"><img src="img/works/react_menu_app.png" alt="メニュー計算アプリ" /></div>
         <p className="works-name">メニュー計算アプリ</p>
         <p className="works-info">css/React</p>
       </a>
       <a onClick={() => handleClick(<ReactTodo />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/react_todo_app.png"/>
         <div className="works-img"><img src="img/works/react_todo_app.png" alt="ToDoリストアプリ" /></div>
         <p className="works-name">ToDoリストアプリ</p>
         <p className="works-info">css/React</p>
       </a>
       <a onClick={() => handleClick(<ReactThree />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/react_three_pronged.png"/>
         <div className="works-img"><img src="img/works/react_three_pronged.png" alt="三目並べ" /></div>
         <p className="works-name">三目並べ</p>
         <p className="works-info">css/React</p>
       </a>
       <a onClick={() => handleClick(<Postal />)} className="works-item">
+        {/* 画像をプリロード */}
+        <PreloadImage src="img/works/js-postal-code.png"/>
         <div className="works-img"><img src="img/works/js-postal-code.png" alt="住所検索アプリ" /></div>
         <p className="works-name">住所検索アプリ</p>
         <p className="works-info">css/JavaScript</p>
