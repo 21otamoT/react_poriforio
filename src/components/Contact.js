fc47daf260d1c5599badd9825084b38cc39ab35f
hirose <yukihio76@gmail.com>
2024-03-03 14:35:37 +0900

    addform

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,16 +6,17 @@ const Contact = () => {
       <h2>お問い合わせ</h2>
       <div className="contact-list">
           <div>
-            <p>
+            {/* <p>
               お問い合わせは、<span className="line">メールまたはSNS</span>にてお願いいたします。
             </p>
             <h3>メール</h3>
-            <p>yukihio76@gmail.com</p>
-            <h3>SNS</h3>
+            <p>yukihio76@gmail.com</p> */}
+            {/* <h3>SNS</h3>
             <a href="https://twitter.com/otamoT10010">
               <img src="img/icons8-x-100.png" alt="エックス（旧ツイッター）" />
               <p>X(旧Twitter)</p>
-            </a>
+            </a> */}
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSexU622SxdyBUvMUau0z5JNubhHCEM2Kwdf3nX9mytPVjEqHg/viewform?embedded=true" width="640" height="807" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
           </div>
           <blockquote className="twitter-tweet"><p lang="ja" dir="ltr"><a href="https://twitter.com/hashtag/%E4%BB%8A%E6%97%A5%E3%81%AE%E7%A9%8D%E3%81%BF%E4%B8%8A%E3%81%92?src=hash&amp;ref_src=twsrc%5Etfw">#今日の積み上げ</a> メモ<br />総学習時間　５９６時間<br />✅React<br />✅世界一流エンジニアの思考法<a href="https://twitter.com/hashtag/%E9%A7%86%E3%81%91%E5%87%BA%E3%81%97%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%A8%E7%B9%8B%E3%81%8C%E3%82%8A%E3%81%9F%E3%81%84?src=hash&amp;ref_src=twsrc%5Etfw">#駆け出しエンジニアと繋がりたい</a><a href="https://twitter.com/hashtag/React?src=hash&amp;ref_src=twsrc%5Etfw">#React</a><a href="https://twitter.com/hashtag/%E4%BE%8D%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2?src=hash&amp;ref_src=twsrc%5Etfw">#侍エンジニア</a><a href="https://twitter.com/hashtag/%E4%BE%8D%E3%83%86%E3%83%A9%E3%82%B3%E3%83%A4?src=hash&amp;ref_src=twsrc%5Etfw">#侍テラコヤ</a><a href="https://twitter.com/hashtag/%E4%BE%8D%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E5%A1%BE?src=hash&amp;ref_src=twsrc%5Etfw">#侍エンジニア塾</a></p>&mdash; ゆっきー (@otamoT10010) <a href="https://twitter.com/otamoT10010/status/1756331228654121176?ref_src=twsrc%5Etfw">February 10, 2024</a></blockquote>
       </div>
